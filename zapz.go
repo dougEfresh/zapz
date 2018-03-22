7d28a4c7a36ab8e959450bd9d906cd20b8372712
Doug Chimento <dchimento@gmail.com>
2018-03-22 18:19:11 +0200

    Adding tests

--- zapz.go
+++ zapz.go
@@ -11,68 +11,60 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-//
-//
-//
-// Copyright (c) 2018. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-// Morbi non lorem porttitor neque feugiat blandit. Ut vitae ipsum eget quam lacinia accumsan.
-// Etiam sed turpis ac ipsum condimentum fringilla. Maecenas magna.
-// Proin dapibus sapien vel ante. Aliquam erat volutpat. Pellentesque sagittis ligula eget metus.
-// Vestibulum commodo. Ut rhoncus gravida arcu.
 
 package zapz
 
 import (
 	"go.uber.org/zap/zapcore"
-	"fmt"
-	"os"
-	"time"
-	"go.uber.org/zap"
-	"github.com/dougEfresh/logzio-go"
-)
 
+	"io"
+	"time"
+
+	"github.com/dougEfresh/logzio-go"
+	"go.uber.org/zap"
+)
 
 // LogzTimeEncoder format to time.RFC3339Nano
 func LogzTimeEncoder(t time.Time, enc zapcore.PrimitiveArrayEncoder) {
 	enc.AppendString(t.UTC().Format(time.RFC3339Nano))
 }
 
-// Message needs to be the message key
+// Message needs to be the message key for logzio
 var enCfg = zapcore.EncoderConfig{
-		TimeKey:        "ts",
-		LevelKey:       "level",
-		NameKey:        "logger",
-		CallerKey:      "caller",
-		MessageKey:     "message",
-		StacktraceKey:  "stacktrace",
-		LineEnding:     zapcore.DefaultLineEnding,
-		EncodeLevel:    zapcore.LowercaseLevelEncoder,
-		EncodeTime:     LogzTimeEncoder,
-		EncodeDuration: zapcore.SecondsDurationEncoder,
-		EncodeCaller:   zapcore.ShortCallerEncoder,
+	TimeKey:        "ts",
+	LevelKey:       "level",
+	NameKey:        "logger",
+	CallerKey:      "caller",
+	MessageKey:     "message",
+	StacktraceKey:  "stacktrace",
+	LineEnding:     zapcore.DefaultLineEnding,
+	EncodeLevel:    zapcore.LowercaseLevelEncoder,
+	EncodeTime:     LogzTimeEncoder,
+	EncodeDuration: zapcore.SecondsDurationEncoder,
+	EncodeCaller:   zapcore.ShortCallerEncoder,
 }
 
 const defaultType = "zap-logger"
 
 // Zapz struc
 type Zapz struct {
-	lz *logzio.LogzioSender
+	lz    *logzio.LogzioSender
 	level zapcore.Level
 	enCfg zapcore.EncoderConfig
-	typ string
+	typ   string
 }
 
 // New will create a zap logger compatible with logz
-func New(token string, opts ...Option) (*zap.Logger, error)  {
+func New(token string, opts ...Option) (*zap.Logger, error) {
 	logz, err := logzio.New(token)
 	if err != nil {
-		return nil ,err
+		return nil, err
 	}
 	z := &Zapz{
 		lz:    logz,
 		level: zap.InfoLevel,
 		enCfg: enCfg,
-		typ: defaultType,
+		typ:   defaultType,
 	}
 
 	if len(opts) > 0 {
@@ -81,7 +73,7 @@ func New(token string, opts ...Option) (*zap.Logger, error)  {
 		}
 	}
 
-	en := zapcore.NewJSONEncoder(enCfg)
+	en := zapcore.NewJSONEncoder(z.enCfg)
 	return zap.New(zapcore.NewCore(en, z.lz, z.level)).With(zap.String("type", z.typ)), nil
 }
 
@@ -91,21 +83,21 @@ type Option interface {
 }
 
 // SetLevel set the log level
-func SetLevel(l  zapcore.Level) Option {
+func SetLevel(l zapcore.Level) Option {
 	return optionFunc(func(z *Zapz) {
 		z.level = l
 	})
 }
 
 // SetEncodeConfig set the encoder
-func SetEncodeConfig(c  zapcore.EncoderConfig) Option {
+func SetEncodeConfig(c zapcore.EncoderConfig) Option {
 	return optionFunc(func(z *Zapz) {
 		z.enCfg = c
 	})
 }
 
 // SetLogz use this logzsender
-func SetLogz(c  *logzio.LogzioSender) Option {
+func SetLogz(c *logzio.LogzioSender) Option {
 	return optionFunc(func(z *Zapz) {
 		z.lz = c
 	})
@@ -119,11 +111,9 @@ func SetType(ty string) Option {
 }
 
 // WithDebug enables
-func WithDebug() Option {
-	return optionFunc(func (z *Zapz){
-		if e  :=  logzio.SetDebug(os.Stderr)(z.lz) ; e != nil {
-			fmt.Fprintf(os.Stderr, "Error setting debug mode %s", e)
-		}
+func WithDebug(w io.Writer) Option {
+	return optionFunc(func(z *Zapz) {
+		logzio.SetDebug(w)(z.lz)
 	})
 }
 
--- zapz_test.go
+++ zapz_test.go
@@ -11,14 +11,178 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-//
-//
-//
-// Copyright (c) 2018. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-// Morbi non lorem porttitor neque feugiat blandit. Ut vitae ipsum eget quam lacinia accumsan.
-// Etiam sed turpis ac ipsum condimentum fringilla. Maecenas magna.
-// Proin dapibus sapien vel ante. Aliquam erat volutpat. Pellentesque sagittis ligula eget metus.
-// Vestibulum commodo. Ut rhoncus gravida arcu.
 
 package zapz
 
+import (
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+
+	"github.com/dougEfresh/logzio-go"
+	"github.com/magiconair/properties/assert"
+	"go.uber.org/zap/zapcore"
+)
+
+func TestNew(t *testing.T) {
+	var sent []byte
+	ts := httptest.NewUnstartedServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		defer r.Body.Close()
+		sent, _ = ioutil.ReadAll(r.Body)
+	}))
+	ts.Start()
+	defer ts.Close()
+	logz, _ := logzio.New("fake", logzio.SetUrl(ts.URL))
+	z, _ := New("fake", SetLogz(logz))
+
+	z.Info("test")
+	z.Sync()
+	var sentMap map[string]string
+	err := json.Unmarshal(sent, &sentMap)
+	if err != nil {
+		t.Fatal("error on json ", err)
+	}
+	time.Sleep(time.Millisecond * 200)
+	assert.Equal(t, sentMap["type"], "zap-logger")
+	assert.Equal(t, sentMap["level"], "info")
+	assert.Equal(t, sentMap["message"], "test")
+	if len(sentMap["ts"]) == 0 {
+		t.Fatal("wrong ts", sentMap["ts"])
+	}
+}
+
+func TestLevel(t *testing.T) {
+	var sent []byte
+	ts := httptest.NewUnstartedServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		defer r.Body.Close()
+		sent, _ = ioutil.ReadAll(r.Body)
+	}))
+	ts.Start()
+	defer ts.Close()
+	logz, _ := logzio.New("fake", logzio.SetUrl(ts.URL))
+	z, _ := New("fake", SetLogz(logz), SetLevel(zapcore.DebugLevel))
+
+	z.Debug("test")
+	z.Sync()
+	var sentMap map[string]string
+	err := json.Unmarshal(sent, &sentMap)
+	if err != nil {
+		t.Fatal("error on json ", err)
+	}
+	time.Sleep(time.Millisecond * 200)
+	assert.Equal(t, sentMap["type"], "zap-logger")
+	assert.Equal(t, sentMap["level"], "debug")
+	assert.Equal(t, sentMap["message"], "test")
+	if len(sentMap["ts"]) == 0 {
+		t.Fatal("wrong ts", sentMap["ts"])
+	}
+}
+
+func TestType(t *testing.T) {
+	var sent []byte
+	ts := httptest.NewUnstartedServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		defer r.Body.Close()
+		sent, _ = ioutil.ReadAll(r.Body)
+	}))
+	ts.Start()
+	defer ts.Close()
+	logz, _ := logzio.New("fake", logzio.SetUrl(ts.URL))
+	z, _ := New("fake", SetLogz(logz), SetLevel(zapcore.DebugLevel), SetType("tester"))
+
+	z.Debug("test")
+	z.Sync()
+	var sentMap map[string]string
+	err := json.Unmarshal(sent, &sentMap)
+	if err != nil {
+		t.Fatal("error on json ", err)
+	}
+	time.Sleep(time.Millisecond * 200)
+	assert.Equal(t, sentMap["type"], "tester")
+	assert.Equal(t, sentMap["level"], "debug")
+	assert.Equal(t, sentMap["message"], "test")
+	if len(sentMap["ts"]) == 0 {
+		t.Fatal("wrong ts", sentMap["ts"])
+	}
+}
+
+func TestConfig(t *testing.T) {
+
+	var cfg = enCfg
+	cfg.TimeKey = "timestamp"
+	var sent []byte
+	ts := httptest.NewUnstartedServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		defer r.Body.Close()
+		sent, _ = ioutil.ReadAll(r.Body)
+	}))
+	ts.Start()
+	defer ts.Close()
+	logz, _ := logzio.New("fake", logzio.SetUrl(ts.URL))
+	z, _ := New("fake", SetLogz(logz), SetEncodeConfig(cfg))
+
+	z.Info("test")
+	z.Sync()
+	var sentMap map[string]string
+	err := json.Unmarshal(sent, &sentMap)
+	if err != nil {
+		t.Fatal("error on json ", err)
+	}
+	time.Sleep(time.Millisecond * 200)
+	assert.Equal(t, sentMap["type"], "zap-logger")
+	assert.Equal(t, sentMap["level"], "info")
+	assert.Equal(t, sentMap["message"], "test")
+	t.Log("timestamp", sentMap["timestamp"])
+	if len(sentMap["ts"]) > 0 {
+		t.Fatal("wrong ts", sentMap["ts"])
+	}
+
+	if len(sentMap["timestamp"]) == 0 {
+		t.Fatal("wrong timestamp")
+	}
+}
+
+type debugWriter struct {
+	dbg []byte
+}
+
+func (w *debugWriter) Write(p []byte) (int, error) {
+	w.dbg = p
+	return len(p), nil
+}
+
+func TestDebug(t *testing.T) {
+	var sent []byte
+	ts := httptest.NewUnstartedServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		defer r.Body.Close()
+		sent, _ = ioutil.ReadAll(r.Body)
+	}))
+	ts.Start()
+	defer ts.Close()
+	var d = &debugWriter{
+		dbg: make([]byte, 1024),
+	}
+	logz, _ := logzio.New("fake", logzio.SetUrl(ts.URL))
+	z, _ := New("fake", SetLogz(logz), WithDebug(d), SetLevel(zapcore.DebugLevel), SetType("tester"))
+
+	z.Debug("test")
+	z.Sync()
+	var sentMap map[string]string
+	err := json.Unmarshal(sent, &sentMap)
+	if err != nil {
+		t.Fatal("error on json ", err)
+	}
+	time.Sleep(time.Millisecond * 200)
+	assert.Equal(t, sentMap["type"], "tester")
+	assert.Equal(t, sentMap["level"], "debug")
+	assert.Equal(t, sentMap["message"], "test")
+	if len(sentMap["ts"]) == 0 {
+		t.Fatal("wrong ts", sentMap["ts"])
+	}
+}
